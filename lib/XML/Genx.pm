075586f30a4d4875ceb15430fa54f601e1bd4f0c
Dominic Mitchell <dom@happygiraffe.net>
2004-12-04 22:52:38 +0000

    Bump version.

--- lib/XML/Genx.pm
+++ lib/XML/Genx.pm
@@ -3,7 +3,7 @@ package XML::Genx;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 # Use XSLoader first if possible.
 eval {
--- lib/XML/Genx/Simple.pm
+++ lib/XML/Genx/Simple.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use base 'XML::Genx';
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 sub Element {
     my $self = shift;
